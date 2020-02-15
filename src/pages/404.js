e010f0797d67ce14a4fe2278b81a7fc22ba0d36e
khajor <kyji8art@gmail.com>
2020-02-14 18:10:36 -0800

    setting up  404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
+import { Link } from 'gatsby';
 
 function NotFound() {
 	return (
 		<div>
-			<h1> Woops</h1>
-			<p>There is nothing here!</p>
-			<button>Back to home</button>
+			<h1> Woops!</h1>
+			<p>Nothing to see here...</p>
+			<button>
+				<Link to="/">Back to home</Link>
+			</button>
 		</div>
 	);
 }
