7977d2247ed0f6aa250eca14a62458993ec3f73d
khajor <kyji8art@gmail.com>
2020-03-04 23:05:37 -0800

    redesigning about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
 import Layout from '../components/Layout';
 
 function HomePage() {
 	return (
 		<Layout title="About page">
+			<div className="section intro">
+				<div className="intro-wrap">
+					<div>
+						<h2 className="intro-name"><span>UX Designer</span> and <span>artist</span> based in Seattle <span>.</span></h2>
+					</div>
+				</div>
+			</div>
 			<div className="top"></div>
 		</Layout>
 	);
